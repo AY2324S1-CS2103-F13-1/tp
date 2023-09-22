bad87d66f990ed492cf5b6deb740759c642552b3
Geoff <e0772885@u.nus.edu>
2023-09-23 01:18:16 +0800

    add remark command

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -56,6 +56,8 @@ public class CommandTestUtil {
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
+    public static final String VALID_REMARK_AMY = "Hello Jeff";
+    public static final String VALID_REMARK_BOB = "Hello Amy!";
 
     public static final EditCommand.EditPersonDescriptor DESC_AMY;
     public static final EditCommand.EditPersonDescriptor DESC_BOB;
